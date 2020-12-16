dc5ceedd495216ccb738acc54fd4d8c666c842fa
lrhernandes <larachernandes@gmail.com>
2020-12-16 00:37:05 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,92 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Kamla PetStore</title>
+        <link rel="stylesheet" type="text/css" href="styles.css" />
+    </head>
+    <body>
+        <div class="page-wrapper">
+            <div class="horizontal-menu">
+                <div>Kamla <strong>PetStore</strong></div>
+                <div>
+                    <input type="text" placeholder="Encontrar produtos"/>
+                    <button>PESQUISAR</button>
+                </div>
+                <a id="shoping-car-horizontal-mennu-content" href="">Carrinho de compras</a>
+                <a href="">Login</a>
+            </div>
+            <div class="banner">
+            </div>
+            <div class="categories">
+                <a href="">Rações</a>
+                <a href="">Brinquedos</a>
+                <a href="">Medicamentos</a>
+            </div>
+            <div class="products-wrapper">
+                <h4>Produtos em destaque</h4>
+                <ul class="products-grid">
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                    <li class="product-card">
+                        <p>Nome do produto</p>
+                        <p>Valor</p>
+                        <p>Desc</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
